9f955d5bbc8bdfc172c71838fdb3d87a7db51557
maks <wojtekkobylko@onet.pl>
2025-11-24 13:54:30 +0100

    startFragment + botton_nav_menu

--- app/src/main/java/com/example/polskakuchniatradycyjn/MainActivity.kt
+++ app/src/main/java/com/example/polskakuchniatradycyjn/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.polskakuchniatradycyjn
 
 import android.os.Bundle
+import android.service.voice.VoiceInteractionSession
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.polskakuchniatradycyjn.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+            val navHostFragment = supportFragmentManager
+                .findFragmentById(R.id.navHostFragmentContainer) as NavHostFragment
+
+            val navController = navHostFragment.navController
+            binding.bottomNavigationView.setupWithNavController(navController)
+
+
     }
 }
