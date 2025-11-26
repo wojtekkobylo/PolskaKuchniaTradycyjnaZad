a8d4bb4374f4988f356f7375ece5464467d6252f
maks <wojtekkobylko@onet.pl>
2025-11-26 12:27:29 +0100

    custom and ready meals directions

--- app/src/main/java/com/example/polskakuchniatradycyjn/MenuChoiceFragment.kt
+++ app/src/main/java/com/example/polskakuchniatradycyjn/MenuChoiceFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.polskakuchniatradycyjn.databinding.FragmentMenuChoiceBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +19,16 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MenuChoiceFragment : Fragment() {
+
+    private var _binding: FragmentMenuChoiceBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +46,24 @@ class MenuChoiceFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_choice, container, false)
+        _binding = FragmentMenuChoiceBinding.inflate(inflater,container,false   )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.gotoweDaniaButton.setOnClickListener {
+            findNavController().navigate(
+                MenuChoiceFragmentDirections.actionMenuChoiceFragment2ToReadyMealFragment()
+            )
+        }
+        binding.wlasneDaniaButton.setOnClickListener {
+            findNavController().navigate(
+                MenuChoiceFragmentDirections.actionMenuChoiceFragment2ToCustomMealFragment()
+            )
+        }
+
     }
 
     companion object {
--- app/src/main/java/com/example/polskakuchniatradycyjn/ReadyMealFragment.kt
+++ app/src/main/java/com/example/polskakuchniatradycyjn/ReadyMealFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.polskakuchniatradycyjn.databinding.FragmentReadyMealBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +19,15 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ReadyMealFragment : Fragment() {
+
+    private var _binding: FragmentReadyMealBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +45,17 @@ class ReadyMealFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ready_meal, container, false)
+        _binding = FragmentReadyMealBinding.inflate(inflater,container,false  )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.goBackButton2.setOnClickListener {
+            findNavController().navigate(
+                ReadyMealFragmentDirections.actionReadyMealFragmentToMenuChoiceFragment2()
+            )
+        }
     }
 
     companion object {
